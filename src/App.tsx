62624666cca6c2defb669834e134b12b23b0d8c7
namqh <qhnam.67@gmail.com>
2024-12-30 17:17:58 +0700

    init code

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
   const [count, setCount] = useState(0)
 
+  const getCount = () => { return 0 }
+  
   return (
-    <>
+    <div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
