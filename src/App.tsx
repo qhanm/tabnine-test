e4b6813003de21a7028c1afe4108512de68a2cb6
namqh <qhnam.67@gmail.com>
2024-12-31 09:38:38 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,7 @@ import './App.css'
 
 function App(): JSX.Element {
 
-  const [count, setCount] = useState<number>(0);
-
-  const getCount = (): number => { return 0; }
+  const [count, setCount] = useState(0);
 
   return (
     <div>
